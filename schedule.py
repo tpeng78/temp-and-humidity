1016918a5dafe361bdd5b2ae36f862dc9fa723bf
Thomas Peng <tpeng@articulate.com>
2018-12-17 22:58:58 -0500

    add max humidity

--- schedule.py
+++ schedule.py
@@ -1,12 +1,14 @@
 import json
 import datetime
 class scheduleRecord:
-    def __init__(self, starttime, endtime, starthour, minHumidity, minTemp):
+    def __init__(self, starttime, endtime, starthour, minHumidity, maxHumidity, minTemp, maxTemp):
         self.starttime = starttime
         self.endtime = endtime
         self.starthour = starthour
-        self.minHumidity = minHumidity
-        self.minTemp = minTemp
+        self.minHumidity = minHumidity # at or below this, the humidifier will turn on 
+        self.maxHumidity = maxHumidity # at this or above, the humidifier will turn off
+        self.minTemp = minTemp # at or below this, the heater will turn on 
+        self.maxHumidity = maxHumidity # at or below this, the heater will turn off
 
 
 class tempschedule:
@@ -14,10 +16,10 @@ class tempschedule:
     def __init__(self):
         self.scheduletype = "hourly"
         self.myschedules=[]
-        self.myschedules.append(scheduleRecord(0, datetime.datetime.now().timestamp(), 8, 20, 80)); 
-        self.myschedules.append(scheduleRecord(0, datetime.datetime.now().timestamp(), 19, 55, 80)); 
-        self.myschedules.append(scheduleRecord(0, datetime.datetime.now().timestamp(), 7, 10, 80)); 
-        self.myschedules.append(scheduleRecord(0, datetime.datetime.now().timestamp(), 0, 55, 80));         
+        self.myschedules.append(scheduleRecord(0, datetime.datetime.now().timestamp(), 8, 20, 21, 80, 82)); 
+        self.myschedules.append(scheduleRecord(0, datetime.datetime.now().timestamp(), 19, 40, 42, 80, 82)); 
+        self.myschedules.append(scheduleRecord(0, datetime.datetime.now().timestamp(), 7, 10, 11, 80, 82)); 
+        self.myschedules.append(scheduleRecord(0, datetime.datetime.now().timestamp(), 0, 40, 42 80, 82));         
         #print("schedule created")
         #for i in self.myschedules:
         #    print (i.starthour)
