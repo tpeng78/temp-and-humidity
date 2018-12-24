d0eb62c435ec7a8b3e7969d0affebc39e0012422
Thomas Peng <tpeng@articulate.com>
2018-12-24 17:14:19 -0500

    add main function and add tempscale

--- initSettings.py
+++ initSettings.py
@@ -1,50 +1,89 @@
 import json
+import re
 
-config = dict([('minHumidity',53.0),('maxHumidity',55.0),('sensorName','kids-room')])
-try: 
-    f = open("config.json","x")
-    print("config.json does not exist, loading defaults...") 
-    f.write(json.dumps(config))
-except FileExistsError:
-    print("config.json exists")
+def cleanTempScale( inputTempScale ):
+    myTempScale = str(inputTempScale)
+    myTempScale = myTempScale.upper()
 
-    mode = input("What mode do you want to run? simple or schedule?")
-    config["mode"] = mode
-    minHumidity = input("What's the minimum humidity? ")
-    f = open("config.json","w")
-    try:
-        minHumidity = float(minHumidity)
-    except ValueError:
-        print("That's not a number.")
-        exit
-    config["minHumidity"] = minHumidity
+    if len(myTempScale) == 1:
+        if myTempScale == 'C' or myTempScale == 'F':
+            return myTempScale
+    elif len(myTempScale) == 0:
+        myTempScale = 'C'
+    elif len(myTempScale) >= 1:
+        if re.search("CEL", myTempScale) != None:
+            # allow for misspellings
+            myTempScale = 'C'
+        elif re.search("FAH", myTempScale)!= None:
+            # allow for misspellings
+            myTempScale = 'F'
+        else:
+            myTempScale = 'C' #default to Celsius
+        
+    return myTempScale
 
-    maxHumidity = input("What's the maximum humidity? ")
-    try:
-        maxHumidity = float(maxHumidity)
-    except ValueError:
-        print("That's not a number.")
-        exit
-    config["maxHumidity"] = maxHumidity
+def main():     
+    tempscale = 'C'  # C for Celsius or F for Fahrenheit
 
-    minTemp = input("What's the minimum temperature? ")
-    f = open("config.json","w")
-    try:
-        minTemp = float(minTemp)
-    except ValueError:
-        print("That's not a number.")
-        exit
-    config["minTemp"] = minTemp
-
-    maxTemp = input("What's the maximum temperature? ")
-    try:
-        maxTemp = float(maxTemp)
-    except ValueError:
-        print("That's not a number.")
-        exit
-    config["maxTemp"] = maxTemp    
+    config = dict([('minHumidity',53.0),('maxHumidity',55.0),('sensorName','kids-room'), ('mode','simple'), ('maxTemp', 24.0), ('minTemp',20.0),('tempscale','C')])
 
 
-    f.write(json.dumps(config))
+    try: 
+        f = open("config.json","x")
+        print("config.json does not exist, loading defaults...") 
+        f.write(json.dumps(config,indent=4, sort_keys=True))
+    except FileExistsError:
+        print("config.json exists")
+
+
+        tempscale = input("What temperature scale do you want to use Celsius? or Fahrenheit?")
+        tempscale = cleanTempScale(tempscale)
+        print("Ok! using " + tempscale)
+        config["tempscale"] = tempscale 
+
+
+        mode = input("What mode do you want to run? simple, schedule or daynight?")
+        config["mode"] = mode
+        minHumidity = input("What's the minimum humidity? ")
+        f = open("config.json","w")
+        try:
+            minHumidity = float(minHumidity)
+        except ValueError:
+            print("That's not a number.")
+            exit
+        config["minHumidity"] = minHumidity
+
+        maxHumidity = input("What's the maximum humidity? ")
+        try:
+            maxHumidity = float(maxHumidity)
+        except ValueError:
+            print("That's not a number.")
+            exit
+        config["maxHumidity"] = maxHumidity
+
+        minTemp = input("What's the minimum temperature? ")
+        f = open("config.json","w")
+        try:
+            minTemp = float(minTemp)
+        except ValueError:
+            print("That's not a number.")
+            exit
+        config["minTemp"] = minTemp
+
+        maxTemp = input("What's the maximum temperature? ")
+        try:
+            maxTemp = float(maxTemp)
+        except ValueError:
+            print("That's not a number.")
+            exit
+        config["maxTemp"] = maxTemp    
+
+
+        f.write(json.dumps(config, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
+
 
 
